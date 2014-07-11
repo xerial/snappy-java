e487f823e5aee1a64a3f113253b9b7de35143ec0
Iheanyi Ekechukwu <iekechuk@us.ibm.com>
2014-07-11 13:24:44 -0500

    Add lib/inc_ibm and remove libsnappyjava.so

---
+++ lib/inc_ibm/jni_md.h
@@ -0,0 +1,53 @@
+/*===========================================================================
+ * Licensed Materials - Property of IBM
+ * "Restricted Materials of IBM"
+ * 
+ * IBM SDK, Java(tm) Technology Edition, v7
+ * (C) Copyright IBM Corp. 2014, 2014. All Rights Reserved
+ *
+ * US Government Users Restricted Rights - Use, duplication or disclosure
+ * restricted by GSA ADP Schedule Contract with IBM Corp.
+ *===========================================================================
+ */
+/*
+ * Copyright (c) 1996, 2000, Oracle and/or its affiliates. All rights reserved.
+ * ORACLE PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+#ifndef _JAVASOFT_JNI_MD_H_
+#define _JAVASOFT_JNI_MD_H_
+
+#define JNIEXPORT  __attribute__((__visibility__("default")))
+#define JNIIMPORT
+#define JNICALL
+
+typedef int jint;
+#ifdef _LP64 /* 64-bit Solaris */
+typedef long jlong;
+#else
+/*typedef long long jlong;*/
+#endif
+
+typedef signed char jbyte;
+
+#endif /* !_JAVASOFT_JNI_MD_H_ */
