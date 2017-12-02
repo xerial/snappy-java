8a3538bc51c648ae809e77171202dfa15aad0e99
Taro L. Saito <leo@xerial.org>
2017-12-01 16:26:13 -0800

    Remove unnecessary scala version setting

--- script/travis-deploy.sh
+++ script/travis-deploy.sh
@@ -6,10 +6,10 @@ VERSION=`perl -npe "s/version in ThisBuild\s+:=\s+\"(.*)\"/\1/" version.sbt | se
 # Deploy a snapshot version only for master branch and jdk8
 if [[ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ]]; then 
   if [[ "$TRAVIS_PULL_REQUEST" == "false" ]] && [[ "$VERSION" == *SNAPSHOT ]]; then
-    sbt ++$TRAVIS_SCALA_VERSION "; test; publish";
+    ./sbt "; test; publish";
   else
-    sbt ++$TRAVIS_SCALA_VERSION test;
+    ./sbt test;
   fi;
 else
-  sbt ++$TRAVIS_SCALA_VERSION test;
+  ./sbt test;
 fi;
