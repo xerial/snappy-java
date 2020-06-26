377d3577cab8585a10a88353976718a0c458a75d
Taro L. Saito <leo@xerial.org>
2020-06-25 19:37:19 -0700

    Reformat

--- src/test/scala/org/xerial/snappy/SnappySpec.scala
+++ src/test/scala/org/xerial/snappy/SnappySpec.scala
@@ -6,7 +6,6 @@ import wvlet.log.{LogSupport, Logger}
 import wvlet.log.io.Timer
 
 /**
-  *
   */
 trait SnappySpec extends WordSpec with Matchers with GivenWhenThen with OptionValues with BeforeAndAfter with Timer with LogSupport {
   Logger.setDefaultFormatter(SourceCodeLogFormatter)
